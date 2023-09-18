7e7dc90850d2e23fbe7f2a8296a12ccb1cb1662d
Tanvi Radia <139433835+tanvi-radia@users.noreply.github.com>
2023-09-18 22:55:36 +0530

    Update post-install.js

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra')
 
 try {
-    fs.copySync('./config/config.env.example', './config/config.env')
+    fs.copySync('./config/config.env')
     console.log('Environment file created successfully.')
 } catch (err) {
     console.error(err)
